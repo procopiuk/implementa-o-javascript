75c136f0c7af03b1a6522d0802fd01e1e7de5689
procopiuk <149600991+procopiuk@users.noreply.github.com>
2024-04-11 16:50:42 -0300

    cew

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual= new Date();
 let dataNiver= prompt ("data do próximo niver:");
-dataNiver= new Date (dataNiver + "T 23:59:59");
+dataNiver= new Date(dataNiver + "T23:59:59");
  let diasQueFaltam= math.foor (dataNiver - dataAtual)/86400000);
- document.querySelector ("#dias_restantes").textContent= diasQueFaltam;
+ document.querySelector("#dias_restantes").textContent= diasQueFaltam;
