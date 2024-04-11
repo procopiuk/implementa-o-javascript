3e8db01092abcd473eb9594ec7e960651416a4a8
procopiuk <149600991+procopiuk@users.noreply.github.com>
2024-04-11 16:41:15 -0300

    kme

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const dataAtual= new Date();
 let dataNiver= prompt ("data do próximo niver:");
 dataNiver= new Date (dataNiver + "T 23:59:59");
- 
+ let diasQueFaltam= math.foor (dataNiver - dataAtual)/86400000);
